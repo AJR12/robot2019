2ee72df330e5308650ace281b01398f0b0a013c0
Julio A Zapata <jzapa039@fiu.edu>
2018-09-08 17:43:11 -0400

    alright

--- lib/nav/PrototypeRobot/PrototypeRobot.ino
+++ lib/nav/PrototypeRobot/PrototypeRobot.ino
@@ -6,12 +6,13 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-//#include "VL53L1X.h" //Laser Sensor library
-#include "SparkFun_VL53L1X_Arduino_Library.h"
+#include "VL53L1X.h" //Laser Sensor library
+//#include "SparkFun_VL53L1X_Arduino_Library.h"
 #define OLED_RESET 4
 
 Adafruit_SSD1306 display(OLED_RESET);
-VL53L1X distanceSensor; //laser sensor object
+//VL53L1X distanceSensor; //laser sensor object
+VL53L1X Distance_Sensor;
 
 uint8_t longRange = 2;//range for distance sensor up to 4m
 
@@ -21,9 +22,9 @@ Motor motor3(5, 34, 40);
 Motor motor4(6, 38, 36);
 DistanceClass sensors(31, 33, 41, 39, 37, 35, 42, 43);
 Imu imuSensor;
-robot myRobot(motor1, motor2, motor3, motor4, 255,imuSensor); //robot(motor1, motor2, motor3, motor4, MaxSpeed)
+robot myRobot(motor1, motor2, motor3, motor4, 255, imuSensor); //robot(motor1, motor2, motor3, motor4, MaxSpeed)
 Navigate myNavigate(myRobot, sensors, imuSensor);
-int distance =0;
+int distance = 0;
 
 
 
@@ -31,42 +32,51 @@ int distance =0;
 
 void setup() {
   Wire.begin();
+  Wire.setClock(400000);
   Serial.begin(9600);
+  if (!Distance_Sensor.init())
+  {
+    Serial.println("Failed to initialize VL53L1X Distance_Sensor!");
+    while (1);
+  }
+  Distance_Sensor.setDistanceMode(VL53L1X::Long);
+  Distance_Sensor.setMeasurementTimingBudget(50000);
+  Distance_Sensor.startContinuous(50);
   //initializing IMU
   imuSensor.bno.setExtCrystalUse(true);
   imuSensor.bno.begin();
   //initializing Oled display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  pinMode(48,OUTPUT);
-  digitalWrite(48,HIGH);
+  pinMode(48, OUTPUT);
+  digitalWrite(48, HIGH);
   //initializing laser sensor
-  
-  Serial.println("VL53L1X Qwiic Test");
 
-  if (distanceSensor.begin() == false)
-  {
-    Serial.println("Sensor offline!");
-  }
-  distanceSensor.setDistanceMode(longRange);
+
   delay(2000);
-  
+
 
 }
 
 void loop() {
   //float x = imuSensor.getInfo();
-//  Serial.println(x);
-//  Serial.print("In Main loop x =");
-//  Serial.println(x);
+  //  Serial.println(x);
+  //  Serial.print("In Main loop x =");
+  //  Serial.println(x);
   //Poll for completion of measurement. Takes 40-50ms.
-//  while (distanceSensor.newDataReady() == false)
-//    delay(5);
-//
- distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor 
-  Serial.println(imuSensor.getPhi());
+  //  while (distanceSensor.newDataReady() == false)
+  //    delay(5);
+  //
+  //distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+  //Serial.println(imuSensor.getPhi());
   //myNavigate.moveFwd(45,100);
-//  PrintOled();
+  Serial.println(imuSensor.getPhi());
+  Distance_Sensor.read();
+  Serial.print("Distance(mm): ");
+  Serial.print(Distance_Sensor.ranging_data.range_mm);
+  Serial.println();
+  
+  PrintOled();
 
 
 
