f139c0a645cc0c3b78772d3246275d1e29c8946c
Abdulmueen Alrashide <Abohsan2010@Gmail.com>
2018-09-29 19:28:53 -0400

    commiting

--- lib/nav/PrototypeRobot/PrototypeRobot.ino
+++ lib/nav/PrototypeRobot/PrototypeRobot.ino
@@ -7,10 +7,11 @@
 #include "Navigate.h"
 #include "Imu.h"
 #include <Wire.h>
-#include <Adafruit_GFX.h>
+//#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <PID_v1.h>  // PID
 #define OLED_RESET 4
+
 Adafruit_SSD1306 display(OLED_RESET);
 
 uint8_t longRange = 2;//range for distance sensor up to 4m
@@ -21,87 +22,129 @@ Motor motor3(5, 32, 33);
 Motor motor4(6, 36, 37);
 DistanceClass sensors(31, 33, 41, 39, 37, 35, 42, 43);
 VL53L1X distanceSensor; //laser sensor object
-Imu imuSensor;
-robot myRobot(motor1, motor2, motor3, motor4, 255, imuSensor); //robot(motor1, motor2, motor3, motor4, MaxSpeed)
-Navigate myNavigate(myRobot, sensors, imuSensor);
+//Imu imuSensor;
+robot myRobot(motor1, motor2, motor3, motor4, 255); //robot(motor1, motor2, motor3, motor4, MaxSpeed)
+//Navigate myNavigate(myRobot, sensors, imuSensor);
 ///  PID veriable
 boolean findd = true;
 double setPoint;
 double input;
 double output1;
 double output2;
-double kp = 5, ki = 0, kd = 0;
+double kp = 7, ki = 0, kd = 0 ;
 
 PID myPID(&input, &output1, &setPoint, kp, ki, kd, DIRECT);
 
 //// End of the virable
 void setup() {
-  Serial.begin(9600);
-  imuSensor.bno.setExtCrystalUse(true);
-  imuSensor.bno.begin();
+  Serial.begin(115200);
+  
+//  imuSensor.bno.setExtCrystalUse(true);
+//  imuSensor.bno.begin();
+  
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   Serial.println("VL53L1X Qwiic Test");
-  if (distanceSensor.begin() == false)
+  if (!distanceSensor.begin())
   {
     Serial.println("Sensor offline!");
   }
   distanceSensor.setDistanceMode(longRange);
+
   ///PID setup
   setPoint = 90;
   myPID.SetMode(AUTOMATIC);
   myPID.SetTunings(kp, ki, kd);
-
   //// end of setup PID
-
+  myRobot.initialize();
 }
 
+//double integral;
+double angle;
+double error = 0;
 void loop() {
-
-
+  
+  myRobot.moveInCircle(0.1);
+//  Serial.println("G");
   displayy();
+  
+//
+//  if (findd) {
+//    findthephi(setPoint);
+//    findd = false;
+//  }
+//  setPoint = wrap(setPoint + 3, 0, 360);
+//  updateError();
+//  double diff = error * 5;
+//  double max_v = 100;
+//  if (diff > max_v)diff = max_v; else if (diff < -max_v)diff = -max_v;
+//
+//  displayy();
+//
+//  myRobot.forwardAbdul( 150 - diff , 150 + diff);
 
-  if(findd){
-    findthephi(setPoint);
-    findd=false;
-  }
+//}
 
-  input = map(imuSensor.getPhi(), 0 , 360, 0, 255);
-  //
-  //  input = map(100, 0 , 360, 0, 255);
-  //  input = imuSensor.getPhi();
-  myPID.Compute();
-  //  Serial.println(output1);
+//void updateError(){
+//  angle = imuSensor.getPhi();
+//  error = wrap(angle-setPoint, -180, 180);
+//}
+
+//void findthephi(int phi) {
+//  int find_accuracy = 2;
+//  while (abs(error) > find_accuracy ) {
+//    updateError();
+//    if (error > 0)
+//      myRobot.rotateLeft(100);
+//    else if (error < 0)
+//      myRobot.rotateRight(100);
+//  }
 
 
-  myRobot.forwardAbdul( 150 , 150 + output1);
+//  while (angle < phi - find_accuracy && angle > phi + find_accuracy ) {
+//    angle = imuSensor.getPhi();
+//    if ((phi + 180) < 360) {
+//      if (angle > (phi - find_accuracy) && angle < (phi + 180)) {
+//        myRobot.rotateRight(100);
+//      } else {
+//        myRobot.rotateLeft(100);
+//      }
+//    } else if ((phi - 180) > 0) {
+////      if (angle < (phi + find_accuracy) && angle > (phi - 180)) {
+////        myRobot.rotateRight(100);
+////      } else {
+////        myRobot.rotateLeft(100);
+////      }
+////    }
+////  }
 
-//  Serial.println(60 + output1);
-
-  //      Serial.println(imuSensor.getPhi());
-  //      Serial.print("  ");
-  //      Serial.print(output1+60);
-  //      Serial.println();
-  //    Serial.println(output2);
-
-}
-void findthephi(int phi) {
-  while (imuSensor.getPhi() < phi ) {
-    myRobot.rotateLeft(100);
-  }
   myRobot.stopp();
 }
 
+void goStraight(int angle){
+  
+}
+void goInCircle(){
+  
+}
+double wrap(double val, double min, double max){
+  double range = max - min;
+  if (val < min)
+    val += range;
+  else if (val > max)
+    val -= range;
+  return val;
+}
 
 void displayy() {
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.print("Hello John");
+  display.print(myRobot.error);
   display.setCursor(0, 20);
-  display.print(imuSensor.getPhi());
+  display.print(myRobot.angle);
   display.setCursor(0, 40);
-  display.print(distanceSensor.getDistance());
+  display.print(myRobot.desireAngle);
   display.display();
 }
 
--- lib/nav/PrototypeRobot/robot.h
+++ lib/nav/PrototypeRobot/robot.h
@@ -7,11 +7,11 @@
 class robot
 {
   public:
-  robot();
-    robot(Motor motor1, Motor motor2, Motor motor3, Motor motor4, int Max, Imu);
+    robot();
+    robot(Motor motor1, Motor motor2, Motor motor3, Motor motor4, int Max);
     void align(int);
-    void forwards(int,int);
-    void forwardAbdul(int,int);
+    void forwards(int, int);
+    void forwardAbdul(int, int);
     void backwards(int);
     void stopp();
     void right(int);
@@ -23,6 +23,13 @@ class robot
     void diagonalRightUp(int);
     void rightWheels(int);
     void leftWheels(int);
+    void moveInCircle(double);
+    void updateError();
+    double wrap(double val, double min, double max);
+    void initialize();
+    double angle ;
+    double error ;
+    double desireAngle;
   private:
     moveInCircular1 (int, int);
     Motor _M1;
@@ -32,6 +39,8 @@ class robot
     Imu imuSensor;
     int maxSpeed;
     float phi;
+    
+
 
 
 
