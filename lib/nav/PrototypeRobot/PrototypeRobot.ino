58f859fa723eea664e2b76ef3cf99fd3ae166bcf
AJR12 <you@example.comantwon510@yahoo.com>
2019-01-10 20:06:04 -0500

    commiting

--- lib/nav/PrototypeRobot/PrototypeRobot.ino
+++ lib/nav/PrototypeRobot/PrototypeRobot.ino
@@ -47,20 +47,6 @@ Motor motor1(7, 24, 25);
 Motor motor2(6, 28, 29);
 Motor motor3(9, 34, 35);
 Motor motor4(8, 38, 39);
-//robot myRobot(motor1, motor2, motor3, motor4, 255, imuSensor); //robot(motor1, motor2, motor3, motor4, MaxSpeed)
-
-<<<<<<< HEAD
-Motor motor1(4, 22, 26);
-Motor motor2(5, 34, 30);
-Motor motor3(6, 38, 42);
-Motor motor4(7, 52, 48);
-DistanceClass sensors(31, 33, 41, 39, 37, 35, 42, 43);
-VL53L1X distanceSensor; //laser sensor object6
-Imu imuSensor;
-robot myRobot(motor1, motor2, motor3, motor4, 255, imuSensor); //robot(motor1, motor2, motor3, motor4, MaxSpeed)
-Navigate myNavigate(myRobot, sensors, imuSensor);
-=======
->>>>>>> antoniosbranch
 
 
 void setup() {
@@ -92,58 +78,11 @@ void setup() {
 
 void loop() {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  //  float phi = imuSensor.getPhi();
-  //  float distance = distanceSensor.getDistance();
-  Serial.println("Hello World");
-  Serial.print("phi");
-  Serial.println(imuSensor.getPhi());
-  //  Serial.print("distance=");
-  //  Serial.println(distance);
-
-
-myRobot.forwards(150);
-
-  //myRobot.align(45);
-  //}
-
-
-//  for (int i = 0; i < 100; i++) {
-//
-//    switch (i % 4) {
-//      case 0:
-//        myRobot.align(45);
-//        delay(1000);
-//        break;
-//      case 1:
-//        myRobot.align(135);
-//        delay(1000);
-//        break;
-//      case 2:
-//        myRobot.align(225);
-//        delay(1000);
-//        break;
-//      case 3:
-//        Serial.println("Entering case 3 in Main");
-//        myRobot.align(315);
-//        Serial.println("Left case 3 in Main");
-//        delay(1000);
-//
-//        break;
-//      default:
-//        myRobot.stopp();
-//    }
-//  }
-=======
-=======
-
   motor1.forward(255);
   motor2.forward(255);
   motor3.forward(255);
   motor4.forward(255);
 
->>>>>>> antoniosbranch
   int disF = lasers.sensF();
   int disB = lasers.sensB();
   int disL = lasers.sensL();
@@ -170,16 +109,6 @@ myRobot.forwards(150);
 
 }
 
-<<<<<<< HEAD
-  //
-  //    float phi = imuSensor.getPhi();
-  //  Serial.print("phi");
-  //  Serial.println(imuSensor.getPhi());
-  //    Serial.print("distance=");
-  ////    Serial.println(distance);
-  //  myRobot.forwards(100);
->>>>>>> antoniosbranch
-=======
 void rpm() {
 
   currentMillis = millis();
@@ -203,7 +132,7 @@ void rpm() {
     encoderValue3 = 0;
     encoderValue4 = 0;
   }
->>>>>>> antoniosbranch
+
 }
 
 
